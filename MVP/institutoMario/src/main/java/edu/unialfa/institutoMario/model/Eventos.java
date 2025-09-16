b612c4394e8444483c4264ee3210a69c2dddabcf
Mateuscheki <mateus13032000@gmail.com>
2025-09-16 17:25:04 -0300

    model Eventos

---
+++ MVP/institutoMario/src/main/java/edu/unialfa/institutoMario/model/Eventos.java
@@ -0,0 +1,30 @@
+package edu.unialfa.institutoMario.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Eventos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nomeEvento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_turma")
+    @JsonManagedReference
+    private Turma turma;
+
+    private LocalDate data;
+
+    private String local;
+
+    private String observacão;
+
+    private String imagemUrl;
+}
